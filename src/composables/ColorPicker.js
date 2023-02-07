5da8193eddbef5686f371961248b4051ecd48da9
Randy <monyerandy15@gmail.com>
2023-02-07 20:56:38 +0100

    extracted color picker composibles

---
+++ src/composables/ColorPicker.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ref, reactive } from "@vue/reactivity";
+
+export default ColorPicker = () => {
+    const colors = ["green", "red", "blue", "purple"];
+    let message = ref("Pick a color...");
+
+    const matchColor = (value) => {
+        const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+        if (colors[randomNumber] === value) {
+            message.value = `You win... [answer: ${colors[randomNumber]}]`;
+            return;
+        }
+
+        message.value = `You loose [answer: ${colors[randomNumber]}]`;
+    }
+    return { colors, message, matchColor }
+}
